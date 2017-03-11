ac4567bd602d8a336b8272cd1a8c36d9c42337c3
Eric Neunaber <eneunaber@gmail.com>
2017-03-10 22:09:38 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace fangraph_priceguide_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Begin....");
+            Console.WriteLine("End....");
+        }
+    }
+}
