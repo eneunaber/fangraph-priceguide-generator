4b52f7cdc429736be5d9a07bd42bb0e80f70d1a0
Eric Neunaber <eneunaber@gmail.com>
2017-03-14 22:54:27 -0500

    Reading conversion file and adding records to list

---
+++ MasterConversionRecord.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace fangraph_priceguide_generator
+{
+    public class MasterConversionRecord
+    {
+        public int? mlb_id {get;set;}	
+        public string mlb_name {get;set;}	
+        public string mlb_pos {get;set;}	
+        public string mlb_team {get;set;}	
+        public string mlb_team_long {get;set;}	
+        public string bats {get;set;}	
+        public string throws {get;set;}	
+        public int? birth_year {get;set;}	
+        public int? bp_id {get;set;}	
+        public string bref_id {get;set;}	
+        public string bref_name {get;set;}	
+        public int? cbs_id {get;set;}
+        public string cbs_name {get;set;}
+        public string	cbs_pos {get;set;}
+        public int?	espn_id {get;set;}	
+        public string espn_name {get;set;}	
+        public string espn_pos {get;set;}	
+        public string fg_id {get;set;}
+        public string	fg_name {get;set;}	
+        public string lahman_id {get;set;}	
+        public int? nfbc_id {get;set;}	
+        public string nfbc_name {get;set;}	
+        public string nfbc_pos {get;set;}	
+        public string retro_id {get;set;}	
+        public string retro_name {get;set;}	
+        public string debut {get;set;}
+        public int? yahoo_id {get;set;}	
+        public string yahoo_name {get;set;}	
+        public string yahoo_pos {get;set;}	
+        public string mlb_depth {get;set;}	
+        public int? ottoneu_id {get;set;}	
+        public string ottoneu_name {get;set;}	
+        public string ottoneu_pos {get;set;}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using CsvHelper;
 
 namespace fangraph_priceguide_generator
 {
@@ -7,6 +11,23 @@ namespace fangraph_priceguide_generator
         static void Main(string[] args)
         {
             Console.WriteLine("Begin....");
+            try {
+                var idMapFileName = args.GetValue(0).ToString();
+                var idConversionPath = args.GetValue(1).ToString();
+                Console.WriteLine("name: {0}", idMapFileName);
+                Console.WriteLine("path: {0}", idConversionPath);
+
+                List<MasterConversionRecord> records;
+
+                using (TextReader reader = File.OpenText(idMapFileName)) {
+                    var csv = new CsvReader( reader );
+                    records = csv.GetRecords<MasterConversionRecord>().ToList();
+                }
+                Console.WriteLine("records.count {0}", records.Count);
+            } catch(Exception ex){
+                Console.WriteLine("Except caught:" + ex.Message);
+                Console.WriteLine(ex.Data["CsvHelper"]);
+            }
             Console.WriteLine("End....");
         }
     }
