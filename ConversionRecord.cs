a8eb4c038480232509dc1fec0d1f10e817e2839c
Eric Neunaber <eneunaber@gmail.com>
2017-03-14 23:29:15 -0500

    Creating ConversionRecord from master records

---
+++ ConversionRecord.cs
@@ -0,0 +1,17 @@
+namespace fangraph_priceguide_generator
+{
+    public class ConversionRecord
+    {
+        public int mlbamID {get; set;}
+        public string Name {get; set;}
+        public string BDB {get; set;}
+        public string statsID {get; set;}
+        public string cbsID {get; set;}
+        public string espnFantasyID {get; set;}
+        public string espnID {get; set;}
+        public string retroID {get; set;}
+        public string Pos_Class {get; set;}
+        public string bisID {get; set;}
+        public string team {get; set;}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 using CsvHelper;
+using AutoMapper;
 
 namespace fangraph_priceguide_generator
 {
@@ -24,6 +25,23 @@ namespace fangraph_priceguide_generator
                     records = csv.GetRecords<MasterConversionRecord>().ToList();
                 }
                 Console.WriteLine("records.count {0}", records.Count);
+
+                Mapper.Initialize(cfg => 
+                    cfg.CreateMap<MasterConversionRecord, ConversionRecord>()
+                        .ForMember(dest => dest.mlbamID, opt => opt.MapFrom(src => src.mlb_id))
+                        .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.mlb_name))
+                        .ForMember(dest => dest.BDB, opt => opt.MapFrom(src => src.bref_id))
+                        .ForMember(dest => dest.statsID, opt => opt.MapFrom(src => src.nfbc_id))
+                        .ForMember(dest => dest.cbsID, opt => opt.MapFrom(src => src.cbs_id))
+                        .ForMember(dest => dest.espnID, opt => opt.MapFrom(src => src.espn_id))
+                        .ForMember(dest => dest.retroID, opt => opt.MapFrom(src => src.retro_id))
+                        .ForMember(dest => dest.Pos_Class, opt => opt.MapFrom(src => src.yahoo_pos))
+                        .ForMember(dest => dest.bisID, opt => opt.MapFrom(src => src.fg_id))
+                        .ForMember(dest => dest.team, opt => opt.MapFrom(src => src.mlb_team))
+                );
+
+                List<ConversionRecord> conversionRecords = Mapper.Map<List<MasterConversionRecord>, List<ConversionRecord>>(records);
+                Console.WriteLine("converted records.count {0}", conversionRecords.Count);
             } catch(Exception ex){
                 Console.WriteLine("Except caught:" + ex.Message);
                 Console.WriteLine(ex.Data["CsvHelper"]);
