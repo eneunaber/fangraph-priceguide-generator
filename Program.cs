1297e8671f00355da5e89fda6160a00d972f481d
Eric Neunaber <eneunaber@gmail.com>
2017-03-14 23:43:20 -0500

    switch delimiter to pipe

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ namespace fangraph_priceguide_generator
                         .ForMember(dest => dest.cbsID, opt => opt.MapFrom(src => src.cbs_id))
                         .ForMember(dest => dest.espnID, opt => opt.MapFrom(src => src.espn_id))
                         .ForMember(dest => dest.retroID, opt => opt.MapFrom(src => src.retro_id))
-                        .ForMember(dest => dest.Pos_Class, opt => opt.MapFrom(src => src.yahoo_pos))
+                        .ForMember(dest => dest.Pos_Class, opt => opt.MapFrom(src => src.yahoo_pos.Replace("/", "|")))
                         .ForMember(dest => dest.bisID, opt => opt.MapFrom(src => src.fg_id))
                         .ForMember(dest => dest.team, opt => opt.MapFrom(src => src.mlb_team))
                 );
