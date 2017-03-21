0d1ab018c963e2481dc6bb36ed7a8c9361cf30b6
Eric Neunaber <eneunaber@gmail.com>
2017-03-20 22:23:27 -0500

    refactoring plus changing headers in hitter file

--- Program.cs
+++ Program.cs
@@ -24,57 +24,21 @@ namespace fangraph_priceguide_generator
                 Console.WriteLine("positionsPath: {0}", positionsPath);
                 Console.WriteLine("fgBatter: {0}", fgBatter);
 
-                List<MasterConversionRecord> records = LoadFile<MasterConversionRecord>(idMapFileName);
+                SetUpMappings();
 
-                Mapper.Initialize(cfg =>
-                    cfg.CreateMap<MasterConversionRecord, ConversionRecord>()
-                        .ForMember(dest => dest.mlbamID, opt => opt.MapFrom(src => src.mlb_id))
-                        .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.mlb_name))
-                        .ForMember(dest => dest.BDB, opt => opt.MapFrom(src => src.bref_id))
-                        .ForMember(dest => dest.statsID, opt => opt.MapFrom(src => src.nfbc_id))
-                        .ForMember(dest => dest.cbsID, opt => opt.MapFrom(src => src.cbs_id))
-                        .ForMember(dest => dest.espnID, opt => opt.MapFrom(src => src.espn_id))
-                        .ForMember(dest => dest.retroID, opt => opt.MapFrom(src => src.retro_id))
-                        .ForMember(dest => dest.Pos_Class, opt => opt.MapFrom(src => src.yahoo_pos.Replace("/", "|")))
-                        .ForMember(dest => dest.bisID, opt => opt.MapFrom(src => src.fg_id))
-                        .ForMember(dest => dest.team, opt => opt.MapFrom(src => src.mlb_team))
-                );
+                List<MasterConversionRecord> records = LoadFile<MasterConversionRecord>(idMapFileName);
+                List<LahmanFielding> lahmanRecords = LoadFile<LahmanFielding>(positionsPath);
+                List<FangraphHitterRecord> fgrecords = LoadFile<FangraphHitterRecord>(fgBatter);
 
                 List<ConversionRecord> conversionRecords = Mapper.Map<List<MasterConversionRecord>, List<ConversionRecord>>(records);
                 Console.WriteLine("converted records.count {0}", conversionRecords.Count);
 
                 SaveFile<ConversionRecord>(conversionRecords, idConversionPath);
 
-                List<LahmanFielding> lahmanRecords = LoadFile<LahmanFielding>(positionsPath);
-                List<FangraphHitterRecord> fgrecords = LoadFile<FangraphHitterRecord>(fgBatter);
-
-                fgrecords.ForEach(x =>
-                {
-                    var match = records.FirstOrDefault(y => x.playerid == y.fg_id);
-                    if (match != null)
-                    {
-                        x.defaultPos = match.yahoo_pos.Replace("/", "|");
-                        x.team = match.mlb_team;
-                        x.mlbamID = match.mlb_id;
-                        var lahmanMatch = lahmanRecords.FirstOrDefault(z => match.lahman_id == z.playerID && year == z.yearID);
-                        if (lahmanMatch != null)
-                        {
-                            x.G = lahmanMatch.G_all;
-                            x.G_1B = lahmanMatch.G_1b;
-                            x.G_2B = lahmanMatch.G_2b;
-                            x.G_3B = lahmanMatch.G_3b;
-                            x.G_SS = lahmanMatch.G_ss;
-                            x.G_C = lahmanMatch.G_c;
-                            x.G_CF = lahmanMatch.G_cf;
-                            x.G_LF = lahmanMatch.G_lf;
-                            x.G_RF = lahmanMatch.G_rf;
-                            x.G_OF = lahmanMatch.G_of;
-                            x.league = lahmanMatch.lgID;
-                        }
-                    }
-                });
+                CreateHitterRecord(year, records, lahmanRecords, fgrecords);
 
                 SaveFile<FangraphHitterRecord>(fgrecords, "/Users/eric.neunaber/Downloads/fred.csv");
+                AlterHitterHeaders("/Users/eric.neunaber/Downloads/fred.csv");
             }
             catch (Exception ex){
                 Console.WriteLine("Except caught:" + ex.Message);
@@ -82,6 +46,53 @@ namespace fangraph_priceguide_generator
             }
             Console.WriteLine("End....");
         }
+
+        private static void CreateHitterRecord(int year, List<MasterConversionRecord> records, List<LahmanFielding> lahmanRecords, List<FangraphHitterRecord> fgrecords)
+        {
+            fgrecords.ForEach(x =>
+            {
+                var match = records.FirstOrDefault(y => x.playerid == y.fg_id);
+                if (match != null)
+                {
+                    x.defaultPos = match.yahoo_pos.Replace("/", "|");
+                    x.team = match.mlb_team;
+                    x.mlbamID = match.mlb_id;
+                    var lahmanMatch = lahmanRecords.FirstOrDefault(z => match.lahman_id == z.playerID && year == z.yearID);
+                    if (lahmanMatch != null)
+                    {
+                        x.G = lahmanMatch.G_all;
+                        x.G_1B = lahmanMatch.G_1b;
+                        x.G_2B = lahmanMatch.G_2b;
+                        x.G_3B = lahmanMatch.G_3b;
+                        x.G_SS = lahmanMatch.G_ss;
+                        x.G_C = lahmanMatch.G_c;
+                        x.G_CF = lahmanMatch.G_cf;
+                        x.G_LF = lahmanMatch.G_lf;
+                        x.G_RF = lahmanMatch.G_rf;
+                        x.G_OF = lahmanMatch.G_of;
+                        x.league = lahmanMatch.lgID;
+                    }
+                }
+            });
+        }
+
+        private static void SetUpMappings()
+        {
+            Mapper.Initialize(cfg =>
+                cfg.CreateMap<MasterConversionRecord, ConversionRecord>()
+                    .ForMember(dest => dest.mlbamID, opt => opt.MapFrom(src => src.mlb_id))
+                    .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.mlb_name))
+                    .ForMember(dest => dest.BDB, opt => opt.MapFrom(src => src.bref_id))
+                    .ForMember(dest => dest.statsID, opt => opt.MapFrom(src => src.nfbc_id))
+                    .ForMember(dest => dest.cbsID, opt => opt.MapFrom(src => src.cbs_id))
+                    .ForMember(dest => dest.espnID, opt => opt.MapFrom(src => src.espn_id))
+                    .ForMember(dest => dest.retroID, opt => opt.MapFrom(src => src.retro_id))
+                    .ForMember(dest => dest.Pos_Class, opt => opt.MapFrom(src => src.yahoo_pos.Replace("/", "|")))
+                    .ForMember(dest => dest.bisID, opt => opt.MapFrom(src => src.fg_id))
+                    .ForMember(dest => dest.team, opt => opt.MapFrom(src => src.mlb_team))
+            );
+        }
+
         public static List<T> LoadFile<T>(string fileLocation) {
             List<T> records = new List<T>();
             Console.WriteLine("Begin reading for file: {0}", fileLocation);
@@ -102,6 +113,24 @@ namespace fangraph_priceguide_generator
                 csv.WriteRecords(records);
                 Console.WriteLine("....Done writing.....");
             }
+        }        
+        private static void AlterHitterHeaders(string fileLocation)
+        {
+            Console.WriteLine("update replace hitter headers");
+            string tempfile = Path.GetTempFileName();
+            FileStream fileStream = new FileStream(fileLocation, FileMode.Open);
+            FileStream tempStream = new FileStream(tempfile, FileMode.OpenOrCreate);
+            
+            using (StreamWriter writer = new StreamWriter(tempStream)) {
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    var firstLine = reader.ReadLine().Replace("Doubles", "2B").Replace("Triples","3B");
+                    writer.WriteLine(firstLine);
+                    while (!reader.EndOfStream)
+                        writer.WriteLine(reader.ReadLine());
+                }
+            }
+            File.Copy(tempfile, fileLocation, true);
         }        
 
     }
