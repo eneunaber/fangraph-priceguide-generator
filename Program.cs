2ad92123b736ea6cc93a060cc75ba7afa0a41d48
Eric Neunaber <eneunaber@gmail.com>
2017-03-23 22:30:40 -0500

    more refactoring

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
 using System.Collections.Generic;
 using AutoMapper;
 using fangraph_priceguide_generator.Strategy;
@@ -15,32 +13,34 @@ namespace fangraph_priceguide_generator
             try
             {
                 var year = Convert.ToInt32(args.GetValue(0));
-                var idMapFileName = args.GetValue(1).ToString();
-                var idConversionPath = args.GetValue(2).ToString();
-                var positionsPath = args.GetValue(3).ToString();
+                var masterCSVFileLocation = args.GetValue(1).ToString();
+                var lahmanAppearancesFileLocation = args.GetValue(2).ToString();
+                var saveLocation = args.GetValue(3).ToString();
 
-                Console.WriteLine("name: {0}", idMapFileName);
-                Console.WriteLine("path: {0}", idConversionPath);
-                Console.WriteLine("positionsPath: {0}", positionsPath);
+                Console.WriteLine("==== INPUTS ====");
+                Console.WriteLine("year: {0}", year);
+                Console.WriteLine("name: {0}", masterCSVFileLocation);
+                Console.WriteLine("lahmanAppearancesFileLocation: {0}", lahmanAppearancesFileLocation);
+                Console.WriteLine("path: {0}", saveLocation);
 
                 AutoMapperMappings.Setup();
 
-                List<MasterConversionRecord> masterRecords = FileHelper.LoadFile<MasterConversionRecord>(idMapFileName);
-                List<LahmanAppearancesRecord> lahmanRecords = FileHelper.LoadFile<LahmanAppearancesRecord>(positionsPath);
+                List<MasterConversionRecord> masterRecords = FileHelper.LoadFile<MasterConversionRecord>(masterCSVFileLocation);
+                List<LahmanAppearancesRecord> lahmanRecords = FileHelper.LoadFile<LahmanAppearancesRecord>(lahmanAppearancesFileLocation);
                 List<ConversionRecord> conversionRecords = Mapper.Map<List<MasterConversionRecord>, List<ConversionRecord>>(masterRecords);
                 Console.WriteLine("converted records.count {0}", conversionRecords.Count);
-                FileHelper.SaveFile<ConversionRecord>(conversionRecords, idConversionPath);                
+                FileHelper.SaveFile<ConversionRecord>(conversionRecords, saveLocation);                
 
                 var extraDetails = new ExtraDetails { Year = year, MasterRecords = masterRecords, LahmanRecords = lahmanRecords };
                 for(int x = 4; x <= args.Length - 1; x++) {
                     try {
-                        var strategy1 = new FangraphHitterStrategy();
-                        var result = strategy1.LoadCSV(args[x]);
-                        strategy1.WriteCSV(extraDetails,result,"/Users/eric.neunaber/Downloads/fred.csv");
-                    } catch(Exception ex) {
-                        var strategy1 = new FangraphPitcherStrategy();
-                        var result = strategy1.LoadCSV(args[x]);
-                        strategy1.WriteCSV(extraDetails,result,"/Users/eric.neunaber/Downloads/ted.csv");
+                        var strategy = new FangraphHitterStrategy();
+                        var result = strategy.LoadCSV(args[x]);
+                        strategy.WriteCSV(extraDetails,result,"/Users/eric.neunaber/Downloads/fred.csv");
+                    } catch(CsvHelper.CsvMissingFieldException ex) {
+                        var strategy = new FangraphPitcherStrategy();
+                        var result = strategy.LoadCSV(args[x]);
+                        strategy.WriteCSV(extraDetails,result,"/Users/eric.neunaber/Downloads/ted.csv");
                     }
                 }
             }
