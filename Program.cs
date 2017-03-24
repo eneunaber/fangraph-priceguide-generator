5860c8a3618abc6bade62569dfc7ca605faba6e7
Eric Neunaber <eneunaber@gmail.com>
2017-03-23 22:43:12 -0500

    rename variable to better show intent

--- Program.cs
+++ Program.cs
@@ -13,13 +13,13 @@ namespace fangraph_priceguide_generator
             Console.WriteLine("Begin....");
             try
             {
-                var year = Convert.ToInt32(args.GetValue(0));
+                var previousYear = Convert.ToInt32(args.GetValue(0));
                 var masterCSVFileLocation = args.GetValue(1).ToString();
                 var lahmanAppearancesFileLocation = args.GetValue(2).ToString();
                 var saveLocation = args.GetValue(3).ToString();
 
                 Console.WriteLine("==== INPUTS ====");
-                Console.WriteLine("year: {0}", year);
+                Console.WriteLine("previousYear: {0}", previousYear);
                 Console.WriteLine("name: {0}", masterCSVFileLocation);
                 Console.WriteLine("lahmanAppearancesFileLocation: {0}", lahmanAppearancesFileLocation);
                 Console.WriteLine("path: {0}", saveLocation);
@@ -32,7 +32,7 @@ namespace fangraph_priceguide_generator
                 Console.WriteLine("converted records.count {0}", conversionRecords.Count);
                 FileHelper.SaveFile<ConversionRecord>(conversionRecords, Path.Combine(saveLocation,"IdConversion-FULL-New.csv"));                
 
-                var extraDetails = new ExtraDetails { Year = year, MasterRecords = masterRecords, LahmanRecords = lahmanRecords };
+                var extraDetails = new ExtraDetails { Year = previousYear, MasterRecords = masterRecords, LahmanRecords = lahmanRecords };
                 for(int x = 4; x <= args.Length - 1; x++) {
                     try {
                         var strategy = new FangraphHitterStrategy();
