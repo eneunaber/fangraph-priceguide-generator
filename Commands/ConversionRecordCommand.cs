30c6a3c4bab5d9643ff41b8621a8a21f602c37c1
Eric Neunaber <eneunaber@gmail.com>
2018-03-18 15:56:21 -0500

    fix bug with wrong variable use

--- Commands/ConversionRecordCommand.cs
+++ Commands/ConversionRecordCommand.cs
@@ -35,7 +35,7 @@ namespace fangraph_priceguide_generator.Commands
         public ConversionRecordCommand(CommandOption optionMasterCSV, CommandOption optionSaveLocation)
         {
             _optionMasterCSV = optionMasterCSV;
-            _optionMasterCSV = optionSaveLocation;
+            _optionSaveLocation = optionSaveLocation;
         }
 
         public void Run()
@@ -44,7 +44,7 @@ namespace fangraph_priceguide_generator.Commands
             string saveLocation = _optionSaveLocation.Value() ?? string.Empty;
             List<MasterConversionRecord> masterRecords = FileHelper.LoadFile<MasterConversionRecord>(masterCSVFileLocation);
             List<ConversionRecord> conversionRecords = Mapper.Map<List<MasterConversionRecord>, List<ConversionRecord>>(masterRecords);
-            FileHelper.SaveFile<ConversionRecord>(conversionRecords, Path.Combine(saveLocation,"IdConversion-FULL-New.csv"));  
+            FileHelper.SaveFile<ConversionRecord>(conversionRecords, Path.Combine(saveLocation,"IdConversion-FULL-New.csv")); 
             // Console.WriteLine("conversion-record......");
             // Console.WriteLine("optionMasterCSV:" + optionMasterCSV.Value());
             // Console.WriteLine("optionSaveLocation:" + optionSaveLocation.Value());
